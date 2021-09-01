3c66029545efcf6bbec1ee836ee1431945fcf613
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-01 18:52:25 -0300

    Configurado API com sucesso

--- nextauth/src/contexts/AuthContext.tsx
+++ nextauth/src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
   email: string;
@@ -24,7 +25,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   //A função tem q ser async por causa que retorno uma promisse
   async function signIn({ email, password }: SignInCredentials) {
-    console.log(email, password);
+    //Tratamento de erro
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <AuthContext.Provider value={{ signIn, isAuthenticated }}>
