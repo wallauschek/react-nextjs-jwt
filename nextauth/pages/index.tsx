f6f75d582b4155c537b241c8ebce88ca27ff0b2f
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-31 21:36:21 -0300

    Finalizado contexto para criação de autenticação

--- nextauth/pages/index.tsx
+++ nextauth/pages/index.tsx
@@ -1,19 +1,21 @@
-import Head from "next/head";
-import Image from "next/image";
-import { FormEvent, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  function handleSubmit(event: FormEvent) {
+
+  const { signIn } = useContext(AuthContext);
+
+  async function handleSubmit(event: FormEvent) {
     event.preventDefault();
     const data = {
       email,
       password,
     };
 
-    console.log(data);
+    await signIn(data);
   }
   return (
     <form onSubmit={handleSubmit} className={styles.container}>
