795c0b29dcb33644017676e0937793520c9d40b0
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-31 21:31:15 -0300

    Iniciando contexto

---
+++ nextauth/contexts/AuthContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, ReactNode } from "react";
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderProps = {
+  // tipagem quando o compoente pode receber qualquer coisa dentro dele
+  children: ReactNode;
+};
+
+const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const isAuthenticated = false;
+
+  //A função tem q ser async por causa que retorno uma promisse
+  async function signIn({ email, password }: SignInCredentials) {
+    console.log(email, password);
+  }
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
