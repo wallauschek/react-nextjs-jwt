0d2bc1d286ba97628a428f8acb2c950e4c87b1f2
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-02 20:51:55 -0300

    Ajuste do refreshToken

--- nextauth/src/services/api.ts
+++ nextauth/src/services/api.ts
@@ -1,9 +1,12 @@
 import axios, { AxiosError } from "axios";
+import { request } from "http";
 import { parseCookies, setCookie } from "nookies";
 
 //Deixando variavel como let para poder modicar o valor dela
 // Como por ex: fazendo refresh do token
 let cookies = parseCookies();
+let isRefreshhing = false;
+let failedRequestsQueue = [];
 
 //Executa uma unica vez quando usuário abre a tela
 export const api = axios.create({
@@ -27,31 +30,59 @@ api.interceptors.response.use(
         cookies = parseCookies();
 
         const { "nextauth.refreshToken": refreshToken } = cookies;
+        const originalConfig = error.config;
 
-        api
-          .post("/refresh", {
-            refreshToken, // passando refreshToken para atualizar o token
-          })
-          .then((response) => {
-            const token = response.data.token;
+        if (!isRefreshhing) {
+          isRefreshhing = true;
+          api
+            .post("/refresh", {
+              refreshToken, // passando refreshToken para atualizar o token
+            })
+            .then((response) => {
+              const token = response.data.token;
 
-            //ladoBrowser, nomeToken , valorToken
-            setCookie(undefined, "nextauth.token", token, {
-              maxAge: 60 * 60 * 24 * 30, // 30 days
-              path: "/", // qualquer endereço para aplicação
-            });
-            setCookie(
-              undefined,
-              "nextauth.refreshToken",
-              response.data.refresToken,
-              {
-                maxAge: 60 * 60 * 24 * 30, // 300 days
+              //ladoBrowser, nomeToken , valorToken
+              setCookie(undefined, "nextauth.token", token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: "/", // qualquer endereço para aplicação
-              }
-            );
+              });
+              setCookie(
+                undefined,
+                "nextauth.refreshToken",
+                response.data.refresToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 300 days
+                  path: "/", // qualquer endereço para aplicação
+                }
+              );
 
-            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+              api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+              failedRequestsQueue.forEach((request) =>
+                request.onSuccess(token)
+              );
+              failedRequestsQueue = [];
+            })
+            .catch((err) => {
+              failedRequestsQueue.forEach((request) => request.onFailure(err));
+              failedRequestsQueue = [];
+            })
+            .finally(() => {
+              isRefreshhing = false;
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              originalConfig.headers["Authorization"] = `Bearer ${token}`;
+              resolve(api(originalConfig));
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err);
+            },
           });
+        });
       } else {
         // deslogar o usuário
       }
