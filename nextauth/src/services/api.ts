d3610088b22f6315c0a12170c129197cd4d1eed1
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-02 08:22:20 -0300

    update

--- nextauth/src/services/api.ts
+++ nextauth/src/services/api.ts
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { parseCookies } from "nookies";
 
-const cookies = parseCookies();
+//Deixando variavel como let para poder modicar o valor dela
+// Como por ex: fazendo refresh do token
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -10,3 +12,31 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies["nextauth.token"]}`,
   },
 });
+
+//Esperando a resposta do back-end
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    // console.log(error.response.status);
+    if (error.response.status === 401) {
+      if (error.response.data?.code === "token.expired") {
+        //renovar token
+        cookies = parseCookies();
+
+        const { "nextauth.refreshToken": refreshToken } = cookies;
+
+        api
+          .post("/refresh", {
+            refreshToken,
+          })
+          .then((response) => {
+            const token = response.data.token;
+          });
+      } else {
+        // deslogar o usuário
+      }
+    }
+  }
+);
