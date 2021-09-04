f9a85ea2fad0aba67cb246744f51005716cc2b5b
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-04 15:45:42 -0300

    Ajustes

--- nextauth/src/utils/withSSRGuest.ts
+++ nextauth/src/utils/withSSRGuest.ts
@@ -9,6 +9,7 @@ import { parseCookies } from "nookies";
  *
  * @function withSSRGuest uma função que recebe outra função
  * Programação funcional/ pesquisar depois
+ * função para usuário não autenticados
  */
 
 export function withSSRGuest<p>(fn: GetServerSideProps<p>) {
@@ -20,7 +21,7 @@ export function withSSRGuest<p>(fn: GetServerSideProps<p>) {
     if (!cookies["nextauth.token"]) {
       return {
         redirect: {
-          destination: "/dashboard,",
+          destination: "/dashboard",
           permanent: false,
         },
       };
