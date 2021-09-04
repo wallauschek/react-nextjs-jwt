77b38440835936fd1c31a31fe416e1bb7e14d16b
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-04 15:31:35 -0300

    Ajustando caso usuário nã esteja logado

---
+++ nextauth/src/utils/withSSRGuest.ts
@@ -0,0 +1,31 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+/**
+ *
+ * @function withSSRGuest uma função que recebe outra função
+ * Programação funcional/ pesquisar depois
+ */
+
+export function withSSRGuest<p>(fn: GetServerSideProps<p>) {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<p>> => {
+    const cookies = parseCookies(ctx);
+
+    if (!cookies["nextauth.token"]) {
+      return {
+        redirect: {
+          destination: "/dashboard,",
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
