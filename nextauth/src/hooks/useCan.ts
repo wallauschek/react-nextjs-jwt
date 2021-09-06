6f760532971f2550674910e6b013481b01d81896
wesley guerra <wesleyguerra9@gmail.com>
2021-09-06 10:16:09 -0300

    Criando hook de permissões

---
+++ nextauth/src/hooks/useCan.ts
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
+type UseCanParams = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function useCan({ permissions, roles }: UseCanParams) {
+  const { isAuthenticated, user } = useContext(AuthContext);
+
+  if (!isAuthenticated) {
+    return false;
+  }
+
+  if (permissions?.length > 0) {
+    //@Every returna true ou false de acordo a condição
+    const hasAllPermissions = permissions.every((permissions) => {
+      return user.permissions.includes(permissions);
+    });
+
+    //Caso não tenha todas permissões return false
+    if (!hasAllPermissions) {
+      return false;
+    }
+  }
+
+  if (roles?.length > 0) {
+    //@Every returna true ou false de acordo a condição
+    const hasAllRoles = permissions.every((role) => {
+      return user.roles.includes(role);
+    });
+
+    //Caso não tenha todas permissões return false
+    if (!hasAllRoles) {
+      return false;
+    }
+  }
+
+  return true;
+}
