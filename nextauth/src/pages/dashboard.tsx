3d206d608dbc3a3be1ff95c6586d6ee6a6ffdbf5
wesley guerra <wesleyguerra9@gmail.com>
2021-09-06 10:38:56 -0300

    Criando components de permissão

--- nextauth/src/pages/dashboard.tsx
+++ nextauth/src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from "next";
 import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
@@ -24,6 +25,9 @@ export default function Dashboard() {
     <>
       <h1>Dashboard: {user?.email}</h1>
       {userCanSeeMetrics && <div>Métricas</div>}
+      <Can permissions={["metrics.list"]}>
+        <h2>Métricas</h2>
+      </Can>
     </>
   );
 }
